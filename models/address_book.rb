0eddb92162aa9e1cd770a701b6b56e05017ae6fe
jbyks <jochen.boeykens@gmail.com>
2017-02-28 11:45:40 +0100

    assignment-8-addressbloc: completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -43,4 +43,12 @@ class AddressBook
     end
     return nil
   end
+  def iterative_search(name)
+    entries.each do |entry|
+      if name == entry.name
+        return entry
+      end
+    end
+    return nil
+  end
 end
